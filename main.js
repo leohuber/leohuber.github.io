2d7fb579f5913587afa7a0f8424ccfddd167b877
Leo Huber <leo_huber@icloud.com>
2024-11-07 19:40:48 +0100

    add animation to greeting functionality in main.js and update index.html

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ import { greet } from './utils.js';
 import anime from './lib/anime.es.js';
 
 greet('World');
+
+anime({
+    targets: 'div',
+    translateX: 250,
+    rotate: '1turn',
+    backgroundColor: '#FFF',
+    duration: 800
+  });
