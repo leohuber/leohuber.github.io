5f1429f255ca8373d721700950550f8bdf4942ce
Leo Huber <leo_huber@icloud.com>
2024-11-19 11:41:31 +0100

    add package.json and webpack configuration; update index.html and utils.js for animation

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-import { greet } from './modules/utils.js';
+import { greet, animateElement } from './modules/utils.js';
 import anime from './lib/anime.es.js';
 
 greet('World');
 
-anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
-  });
+animateElement();
--- modules/utils.js
+++ modules/utils.js
@@ -1,5 +1,20 @@
+import u from '../lib/umbrella.esm.js';
+import anime from '../lib/anime.es.js';
+
 export function greet(name) {
   const greetingElement = document.createElement('p');
   greetingElement.textContent = `Hello, ${name}!`;
   document.body.appendChild(greetingElement);
+}
+
+export function animateElement() {
+  const element = u('#mySvg').first();
+  if (element) {
+    anime({
+      targets: element,
+      translateX: 250,
+      duration: 1000,
+      easing: 'easeInOutQuad'
+    });
+  }
 }
